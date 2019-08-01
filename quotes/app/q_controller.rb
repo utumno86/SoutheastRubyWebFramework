a4aa23853df0f315ea5f0b7b84c98a309c471dcf
mnash <utumno86@gmail.com>
2019-08-01 14:31:21 -0500

    made a controller

---
+++ quotes/app/q_controller.rb
@@ -0,0 +1,5 @@
+class QController < Roolz::Controller
+  def a_quote
+    "What's up, Doc?"
+  end
+end
--- roolz/lib/roolz.rb
+++ roolz/lib/roolz.rb
@@ -3,11 +3,24 @@ require "roolz/version"
 module Roolz
   class App
     def call(env)
-      [200,
-      {'Content-Type' => 'text/html'},
-      ["hello from roolz!!!"]]
+     kl, act = cont_and_act(env)
+     text = kl.new(env).send(act)
+     [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/')
+      con = con.capitalize + "Controller"
+      [Object.const_get(con), act]
     end
   end
+
+   class Controller
+      attr_reader :env
+      def initialize(env)
+        @env = env
+      end
+    end
 
   class Error < StandardError; end
   # Your code goes here...
