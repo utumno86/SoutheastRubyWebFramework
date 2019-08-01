6170a81f1e88d00e7f05407903ffc0fcc83e0841
mnash <utumno86@gmail.com>
2019-08-01 14:38:52 -0500

    wrote the section two test

--- roolz/spec/roolz_spec.rb
+++ roolz/spec/roolz_spec.rb
@@ -1,10 +1,14 @@
+class TedController < Roolz::Controller
+  def think; "Woah, man..."; end
+end
+
 RSpec.describe Roolz do
   it "has a version number" do
     expect(Roolz::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    env = { "PATH_INFO" => "/",
+    env = { "PATH_INFO" => "/ted/think",
     "QUERY_STRING" => "" }
 
     expect(::Roolz::App.new.call(env)[0]).to eq(200)
